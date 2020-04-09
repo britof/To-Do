b7fb7d45cc17a2aae2431e64de82233f92ebc889
britof <41692419+britof@users.noreply.github.com>
2020-04-09 13:11:32 -0300

    To Do List
    Back-end

---
+++ todos.js
@@ -0,0 +1,52 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var ToDos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function render() 
+{
+    listElement.innerHTML = '';
+    for(todo of ToDos) 
+    {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var pos = ToDos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'del(' + pos + ')');
+
+        var linkText = document.createTextNode(' #Delete');
+        linkElement.appendChild(linkText);
+        
+        todoElement.appendChild(todoText);
+        todoElement.append(linkElement);
+        listElement.appendChild(todoElement);
+
+        saveToStorage();
+    }
+}
+render();
+
+function add() 
+{
+    var todoText = inputElement.value;
+    ToDos.push(todoText);
+    inputElement.value = '';
+    render();
+}
+buttonElement.onclick = add;
+
+function del(pos)
+{
+    ToDos.splice(pos, 1);
+    render();
+
+}
+
+function saveToStorage() 
+{
+    localStorage.setItem('list_todos', JSON.stringify(ToDos));
+}
